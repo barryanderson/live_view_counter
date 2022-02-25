01d6215ea11fb0281adbd0cd50df29f7a5c26068
Barry Anderson <barryanderson@users.noreply.github.com>
2022-02-25 16:27:19 +0000

    live counter created

--- config/dev.exs
+++ config/dev.exs
@@ -17,7 +17,7 @@ config :live_view_counter, LiveViewCounterWeb.Endpoint,
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
     esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
-    tailwind: {Tailwind, :install_and_run, [:default, ~w (--watch)]}
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
