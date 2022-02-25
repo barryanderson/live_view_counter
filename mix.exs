4cf3503f48388e22e1597365d1021c8eb5ec50eb
Barry Anderson <barryanderson@users.noreply.github.com>
2022-02-25 15:05:31 +0000

    added tailwind dependency

--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule LiveViewCounter.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:tailwind, "~> 0.1.5"}
     ]
   end
 
